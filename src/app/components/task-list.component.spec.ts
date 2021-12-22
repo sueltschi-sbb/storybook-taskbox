d4987f3afcdc7d6ae5357b56e4e7eabd52015965
u233723 <samuel.ueltschi@sbb.ch>
2021-12-22 15:18:33 +0100

    data

--- src/app/components/task-list.component.spec.ts
+++ src/app/components/task-list.component.spec.ts
@@ -1,15 +1,13 @@
 import { render } from '@testing-library/angular';
-
-import { TaskListComponent } from './task-list.component';
-import { TaskComponent } from './task.component';
-
+import { PureTaskListComponent } from './pure-task-list.component';
 //👇 Our story imported here
-import { WithPinnedTasks } from './task-list.stories';
+import { WithPinnedTasks } from './pure-task-list.stories';
+import { TaskComponent } from './task.component';
 
 describe('TaskList component', () => {
   it('renders pinned tasks at the start of the list', async () => {
     const mockedActions = jest.fn();
-    const tree = await render(TaskListComponent, {
+    const tree = await render(PureTaskListComponent, {
       declarations: [TaskComponent],
       componentProperties: {
         ...WithPinnedTasks.args,
